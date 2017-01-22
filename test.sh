9885e939ff23d9320c7c305ddbdf18b166665362
Eric Wang <ezwang1@gmail.com>
2017-01-22 15:24:35 -0500

    improve test script

--- test.sh
+++ test.sh
@@ -6,12 +6,25 @@ set -e
 cd com.idtus.contest.winter2017.framework
 mvn package
 
+function test_jar() {
+    OUTPUT=$(java -jar target/com.idtus.contest.winter2017.framework.jar -jarToTestPath $1 -toolChain)
+    echo "$OUTPUT"
+    # make sure all tests have passed
+    TOTAL=$(echo "$OUTPUT" | sed -n 's/Total predefined tests run\: \([0-9]\+\)/\1/p')
+    PASSED=$(echo "$OUTPUT" | sed -n 's/Number of predefined tests that passed\: \([0-9]\+\)/\1/p')
+    if [ "$PASSED" -eq "$TOTAL" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
+
 # validate output with provided jars
 echo '=== TesterTypeCheck.jar'
-java -jar target/com.idtus.contest.winter2017.framework.jar -jarToTestPath ../supporting_files/jars/TesterTypeCheck.jar -toolChain
+test_jar ../supporting_files/jars/TesterTypeCheck.jar
 echo '=== RegexPatternMatch.jar'
-java -jar target/com.idtus.contest.winter2017.framework.jar -jarToTestPath ../supporting_files/jars/RegexPatternMatch.jar -toolChain
+test_jar ../supporting_files/jars/RegexPatternMatch.jar
 echo '=== CommandLineEncryption.jar'
-java -jar target/com.idtus.contest.winter2017.framework.jar -jarToTestPath ../supporting_files/jars/CommandLineEncryption.jar -toolChain
+test_jar ../supporting_files/jars/CommandLineEncryption.jar
 echo '=== LeetConverter.jar'
-java -jar target/com.idtus.contest.winter2017.framework.jar -jarToTestPath ../supporting_files/jars/LeetConverter.jar -toolChain
+test_jar ../supporting_files/jars/LeetConverter.jar
